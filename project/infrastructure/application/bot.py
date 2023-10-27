b7be95cf337cab717c52dec95f3a7f285161a0f8
devRSV <devRSV@d.c>
2023-10-28 02:06:45 +0300

    Add token checking

--- project/infrastructure/application/bot.py
+++ project/infrastructure/application/bot.py
@@ -1,6 +1,12 @@
+import logging
 import os
 
 from aiogram import Bot
 from aiogram.enums import ParseMode
+from aiogram.utils.token import TokenValidationError
 
-bot: Bot = Bot(token=os.environ.get('BOT_TOKEN'), parse_mode=ParseMode.HTML)
+try:
+    bot: Bot = Bot(token=os.environ.get('BOT_TOKEN'), parse_mode=ParseMode.HTML)
+except TokenValidationError:
+    logging.error('Token is invalid or empty')
+    exit(1)
